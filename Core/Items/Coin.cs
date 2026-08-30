269dd6177b5b433b2e94fd2baaa55a4d7e83fe03
Edsaje <quentin.beaud@hotmail.fr>
2026-08-30 20:46:57 +0200

    feat: Add pickup cooldown to Coin so player cannot instantly re-collect it

--- Core/Items/Coin.cs
+++ Core/Items/Coin.cs
@@ -13,11 +13,21 @@ namespace KingdomCore.Items
         public float HorizontalSpread { get; set; } = 50.0f;
 
         public int Value { get; set; } = 1;
+        
+        // Bloque le ramassage par le Roi pendant 1 seconde (mais pas par l'esprit !)
+        public bool CanBePickedUpByKing { get; private set; } = false;
 
         public override void _Ready()
         {
             float randomX = (float)GD.RandRange(-HorizontalSpread, HorizontalSpread);
             LinearVelocity = new Vector2(randomX, JumpForce);
+            
+            // On crée un petit chrono (Timer) d'une seconde directement en code
+            GetTree().CreateTimer(1.0f).Timeout += () => 
+            {
+                // Le code dans ce bloc s'exécute après 1 seconde
+                CanBePickedUpByKing = true; 
+            };
         }
     }
 }
